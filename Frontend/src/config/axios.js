2ab41ec9789989fdef7574bb0f09dd86d3f54032
Gaurav Phule <gaurav230605@gmail.com>
2026-01-14 12:10:08 +0530

    fixed axios base url

--- Frontend/src/config/axios.js
+++ Frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Point to your Backend Port
+  baseURL: import.meta.env.VITE_API_URL, // Point to your Backend Port
   withCredentials: true,            // 🍪 THIS FORCES COOKIES
   headers: {
     "Content-Type": "application/json",
